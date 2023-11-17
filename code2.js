7f514a88dc2d0e7e43b81ce0ce1a63e6caac9098
MarioBermu <147144316+MarioBermu@users.noreply.github.com>
2023-11-17 10:36:57 +0100

    06

--- code2.js
+++ code2.js
@@ -1,25 +1,40 @@
 const SWAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com/';
 
 window.onload = async () => {
-  
+
     const Wizards = await getAllWizards();
-    
+
     const spinnerHtmlElement2 = document.getElementById('spinner');
     spinnerHtmlElement2.remove();
-  
+
     for (const Wizard of Wizards) {
-      const mainHtmlElement = document.getElementById('main');
-      const newElement = document.createElement('div');
-      newElement.innerHTML = `
-        <h2>${Wizard.fistName}</h2>
+        const mainHtmlElement = document.getElementById('main');
+        const newElement = document.createElement('div');
+        newElement.innerHTML = `
+        <h2>${Wizard.firstName} ${Wizard.lastName}</h2>
         
-        
-      `;
-      mainHtmlElement.appendChild(newElement);
+      `
+        mainHtmlElement.appendChild(newElement);
+        for (const elixir of Wizard.elixirs) {
+            const mainHtmlElement = document.getElementById('main');
+            const newElement = document.createElement('div');
+            newElement.innerHTML = `
+            <p>${elixir.name}</p>
+            <button onclick="ClickElixir(${elixir.id})" id="elixir">Elixir</button>
+            `
+            
+                ;
+            mainHtmlElement.appendChild(newElement);
+
+
+        };
+    };
+    async function getAllWizards() {
+        const response = await fetch(`${SWAPI_BASE_URL}Wizards`);
+        const data = await response.json();
+        return data;
     }
-  };
-  async function getAllWizards() {
-    const response = await fetch(`${SWAPI_BASE_URL}Wizards`);
-    const data = await response.json();
-    return data;
-  }
+}
+    function ClickElixir(id) {
+        
+    }
