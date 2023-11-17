bd9af7ca3cfa2a2c234c6b59074faae7e88315d3
MarioBermu <147144316+MarioBermu@users.noreply.github.com>
2023-11-17 09:51:53 +0100

    04

--- code.js
+++ code.js
@@ -19,10 +19,37 @@ window.onload = async () => {
   }
 };
 
+
+  window.onload = async () => {
+  
+    const Wizards = await getAllWizards();
+    
+    const spinnerHtmlElement2 = document.getElementById('spinner2');
+    spinnerHtmlElement2.remove();
+  
+    for (const Wizard of Wizards) {
+      const mainHtmlElement = document.getElementById('main');
+      const newElement = document.createElement('div');
+      newElement.innerHTML = `
+        <h2>${Wizard.fistName}</h2>
+        
+        
+      `;
+      mainHtmlElement.appendChild(newElement);
+    }
+  };
+
+
 async function getAllElixirs() {
   const response = await fetch(`${SWAPI_BASE_URL}Elixirs`);
   const data = await response.json();
   return data;
 }
+async function getAllWizards() {
+  const response = await fetch(`${SWAPI_BASE_URL}Wizards`);
+  const data = await response.json();
+  return data;
+}
+
 
 
