2fd76e795ea8f129dc2e8bd7e2611a228b04212c
MarioBermu <147144316+MarioBermu@users.noreply.github.com>
2023-11-17 09:20:26 +0100

    03

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 const SWAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com/';
 
 window.onload = async () => {
-
+  
   const Elixirs = await getAllElixirs();
-
+  
   const spinnerHtmlElement = document.getElementById('spinner');
   spinnerHtmlElement.remove();
 
-  for (const Elixirs of Elixirs) {
+  for (const Elixir of Elixirs) {
     const mainHtmlElement = document.getElementById('main');
     const newElement = document.createElement('div');
     newElement.innerHTML = `
-      <h2>${Elixirs.Name}</h2>
+      <h2>${Elixirs.name}</h2>
       
     `;
     mainHtmlElement.appendChild(newElement);
@@ -21,7 +21,7 @@ window.onload = async () => {
 async function getAllElixirs() {
   const response = await fetch(`${SWAPI_BASE_URL}Elixirs`);
   const data = await response.json();
-  return data.results;
+  return data;
 }
 
 
